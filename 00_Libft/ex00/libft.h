4d03621880c54a6b86cfb23a308f5ff8d483a88e
chichoon <chichoon.choi@gmail.com>
2021-05-03 18:50:20 +0900

    libft

--- 00_Libft/ex00/libft.h
+++ 00_Libft/ex00/libft.h
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 13:59:11 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/05/03 17:32:00 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/05/03 18:49:16 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,22 @@
 
 void	*ft_memset(void *ptr, int value, size_t n);
 void	ft_bzero(void *ptr, size_t n);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void	*ft_memccpy(void *restrict d, const void *restrict s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *src, int c, size_t n);
-
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
-
+size_t	ft_strlcpy(char *dst, const char *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *restrict src, size_t n);
+char	*ft_strchr(const char *src, int c);
+char	*ft_strrchr(const char *src, int c);
+char	*ft_strnstr(const char *src, const char *to_find, size_t n);
+int		atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
