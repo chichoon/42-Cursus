4e7035230969bd89944a85c8adfe70c9c49fcee8
chichoon <chichoon.choi@gmail.com>
2021-05-03 22:47:10 +0900

    strtrim

---
+++ 00_Libft/ex00/ft_strtrim.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/03 22:15:34 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/05/03 22:46:05 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_is_in_set(char s1, char const *set)
+{
+	while (*set)
+	{
+		if (s1 == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char			*start;
+	char			*end;
+	unsigned int	range;
+	char			*ptr;
+
+	start = (char *)s1;
+	end = (char *)s1 + ft_strlen(s1);
+	if (ft_is_in_set(*(end - 1), set))
+		end--;
+	while (ft_is_in_set(*start, set))
+		start++;
+	while (ft_is_in_set(*end, set) && start < end)
+		end--;
+	range = end - start;
+	ptr = malloc(sizeof(char) * range + 1);
+	ft_memcpy(ptr, start, range);
+	ptr += range;
+	*ptr = 0;
+	return (ptr - range);
+}
