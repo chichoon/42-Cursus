bf87db877a6a255b935047aff536798545299078
chichoon <chichoon.choi@gmail.com>
2021-05-03 15:52:06 +0900

    makefile

--- 00_Libft/ex00/src/ft_memcmp.c
+++ 00_Libft/ex00/src/ft_memcmp.c
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   memcmp.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/03 15:17:15 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/05/03 15:38:19 by jiychoi          ###   ########.fr       */
+/*   Created: 2021/05/03 15:39:25 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/05/03 15:44:04 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *src, int c, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *src_backup;
+	unsigned char	*s1_backup;
+	unsigned char	*s2_backup;
 
-	src_backup = (unsigned char *)src;
+	s1_backup = (unsigned char *)s1;
+	s2_backup = (unsigned char *)s2;
 	while (n)
 	{
-		if (*src_backup == (unsigned char)c)
-			return (src_backup);
+		if (*s1_backup != *s2_backup)
+			return (*s1_backup - *s2_backup);
+		s1_backup++;
+		s2_backup++;
 		n--;
 	}
 	return (0);
