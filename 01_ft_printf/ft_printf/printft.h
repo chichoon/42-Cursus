afcf21826689b5fe31d2b90caa2c65ad1e84fdf5
chichoon <chichoon.choi@gmail.com>
2021-05-06 10:46:09 +0900

    circle 1

---
+++ 01_ft_printf/ft_printf/printft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printft.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/06 10:43:09 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/05/06 10:45:42 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTFT_H
+# define PRINTFT_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+
+/*
+	** the most important main function : ft_printf
+*/
+int	ft_printf(const char *, ...);
+
+
+#endif
