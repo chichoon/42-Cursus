68203e4f2404ea042c30f4df5b78f6b12933b156
chichoon <chichoon.choi@gmail.com>
2021-05-24 21:37:17 +0900

    python day01

---
+++ 50_pythonpiscine/day01/ex01/numbers.py
@@ -0,0 +1,9 @@
+def numbers_func():
+	with open('numbers.txt', 'r') as f:
+		line = f.read().strip('\n')
+		numbers = line.split(',')
+		for num in numbers:
+			print(str(num))
+
+if __name__ == '__main__':
+	numbers_func()
---
+++ 50_pythonpiscine/day01/ex04/state.py
@@ -0,0 +1,28 @@
+import sys
+
+def state(arg):
+	states = {
+	"Oregon" : "OR",
+	"Alabama" : "AL",
+	"New Jersey": "NJ",
+	"Colorado" : "CO"
+	}
+	capital_cities = {
+	"OR": "Salem",
+	"AL": "Montgomery",
+	"NJ": "Trenton",
+	"CO": "Denver"
+	}
+	if len(sys.argv) == 2:
+		for key, value in capital_cities.items():
+			if sys.argv[1] == value:
+				for key2, value2 in states.items():
+					if key == value2:
+						print(key2)
+						return
+		print("Unknown state")
+
+if __name__ == '__main__':
+	arg = sys.argv
+	state(arg)
+
