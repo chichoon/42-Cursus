13a60bd2b3552203b262aacbb1661da2bb1a621a
chichoon <chichoon.choi@gmail.com>
2021-05-28 17:35:43 +0900

    ex05

---
+++ 50_pythonpiscine/day03/ex05/my_script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#python3 -m venv django_venv
+source ./django_venv/bin/activate
+#pip3 install --upgrade pip
+#pip install -r requirement.txt
+#django-admin startproject pypiscine
+cd pypiscine
+python3 manage.py migrate
+python3 manage.py runserver
+#python3 manage.py startapp helloworld
+open http://localhost:8000/helloworld/
