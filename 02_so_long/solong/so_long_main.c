09209d980d6d1137d1ea9388246fdb952b4a2ad9
chichoon <chichoon.choi@gmail.com>
2021-06-15 21:10:48 +0900

    solong

--- 02_so_long/solong/so_long_main.c
+++ 02_so_long/solong/so_long_main.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 14:48:13 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/06/15 20:38:28 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/06/15 20:53:36 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,28 @@
 void	so_long(char **map, t_map_info *info)
 {
 	int		temp;
+	int		x;
+	int 	y;
+	void	*img_temp;
 
 	info->mlx_ptr = mlx_init();
 	info->win_ptr = mlx_new_window(info->mlx_ptr, info->map_width * 64,
 		info->map_height * 64, "so_long");
+	img_temp = mlx_xpm_file_to_image(info->mlx_ptr, "char123.xpm", &temp, &temp);
 	info->img_ptr = mlx_xpm_file_to_image(info->mlx_ptr, "charset.xpm",
 		&temp, &temp);
-	printf("width: %zu, height: %zu\n", info->map_width, info->map_height);
+	x = 0;
+	while (x < (int)info->map_width)
+	{
+		y = 0;
+		while (y < (int)info->map_height)
+		{
+			mlx_put_image_to_window(info->mlx_ptr, info->win_ptr, img_temp,
+			x * 64, y * 64);
+			y++;
+		}
+		x++;
+	}
 	sol_draw_corner(info);
 	sol_draw_ywall(info);
 	sol_draw_xwall(info);
