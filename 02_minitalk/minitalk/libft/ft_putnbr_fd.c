9b423104ac6b9fd3dfa24ed3a00fd9db1969da88
chichoon <chichoon.choi@gmail.com>
2021-06-21 19:23:47 +0900

    add libft to minitalk

---
+++ 02_minitalk/minitalk/libft/ft_putnbr_fd.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/04 13:15:23 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/05/08 20:49:08 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	ft_putnbr_recur(unsigned int n, int fd)
+{
+	char	c;
+
+	if (n == 0)
+		return ;
+	ft_putnbr_recur(n / 10, fd);
+	c = n % 10 + '0';
+	write(fd, &c, 1);
+}
+
+void		ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	absol;
+
+	if (n == 0)
+	{
+		write(fd, "0", 1);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		absol = n * (-1);
+	}
+	else
+		absol = n;
+	ft_putnbr_recur(absol, fd);
+}
---
+++ 02_minitalk/minitalk/libft/ft_strlcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/03 15:54:08 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/05/03 21:12:39 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	if (!dst || !src)
+		return (0);
+	while (i + 1 < n)
+	{
+		if (!*src)
+			break ;
+		else
+		{
+			*dst++ = *src++;
+			i++;
+		}
+	}
+	while (*src)
+	{
+		i++;
+		src++;
+	}
+	if (n != 0)
+		*dst = '\0';
+	return (i);
+}
