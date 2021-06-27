b1dd2f38b8c32b5552c5f734ddd8719be395fab6
chichoon <chichoon.choi@gmail.com>
2021-06-27 20:23:36 +0900

    minitalk delete str validation

--- 02_minitalk/minitalk/client/minitalk_client.c
+++ 02_minitalk/minitalk/client/minitalk_client.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 15:09:40 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/06/24 23:32:38 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/06/27 20:22:07 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,20 +94,6 @@ void		client_init_struct(void)
 	sigact_cli_string.sa_handler = client_send_string;
 }
 
-static void	client_validate(int pid, char *str)
-{
-	if (pid < 101 || pid > 99999)
-	{
-		ft_putstr_fd("Invalid PID!\n", 1);
-		exit(1);
-	}
-	if (*str == 0)
-	{
-		ft_putstr_fd("Too short string!\n", 1);
-		exit(1);
-	}
-}
-
 int			main(int argc, char *argv[])
 {
 	if (argc != 3)
@@ -118,7 +104,11 @@ int			main(int argc, char *argv[])
 	g_data_tosend.pid = ft_atoi(argv[1]);
 	g_data_tosend.length = ft_strlen(argv[2]);
 	g_data_tosend.str = argv[2];
-	client_validate(g_data_tosend.pid, argv[2]);
+	if (g_data_tosend.pid < 101 || g_data_tosend.pid > 99999)
+	{
+		ft_putstr_fd("Invalid PID!\n", 1);
+		exit(1);
+	}
 	client_init_struct();
 	sigaction(SIGUSR1, &sigact_cli_connect, 0);
 	kill_and_pause(g_data_tosend.pid, SIGUSR1);
--- 02_push_swap/pushswap/push_swap_linked_list.c
+++ 02_push_swap/pushswap/push_swap_linked_list.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 16:03:18 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/06/26 16:11:58 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/06/26 22:37:10 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,23 @@ t_dnode	*ps_lstadd_front(int key, t_dnode *dnode_next)
 	return node;
 }
 
+void	ps_lstmove_front(t_dnode *dnode_tomove, t_dnode *dnode_next)
+{
+	if (dnode_next->prev == 0)
+	{
+		dnode_tomove->prev = 0;
+		dnode_next->prev = dnode_tomove;
+		dnode_tomove->next = dnode_next;
+	}
+	else
+	{
+		dnode_next->prev->next = dnode_tomove;
+		dnode_tomove->prev = dnode_next->prev;
+		dnode_next->prev = dnode_tomove;
+		dnode_tomove->next = dnode_next;
+	}
+}
+
 t_dnode	*ps_lstadd_back(int key, t_dnode *dnode_prev)
 {
 	t_dnode *node;
@@ -60,6 +77,23 @@ t_dnode	*ps_lstadd_back(int key, t_dnode *dnode_prev)
 	return node;
 }
 
+void	ps_lstmove_back(t_dnode *dnode_tomove, t_dnode *dnode_prev)
+{
+	if (dnode_prev->next == 0)
+	{
+		dnode_tomove->prev = dnode_prev;
+		dnode_tomove->next = 0;
+		dnode_prev->next = dnode_tomove;
+	}
+	else
+	{
+		dnode_prev->next->prev = dnode_tomove;
+		dnode_tomove->prev = dnode_prev;
+		dnode_tomove->next = dnode_prev->next;
+		dnode_prev->next = dnode_tomove;
+	}
+}
+
 void	ps_lstdel(t_dnode *dnode_del)
 {
 	if (dnode_del->prev == 0 && dnode_del->next == 0)
@@ -81,5 +115,19 @@ void	ps_lstdel(t_dnode *dnode_del)
 		dnode_del->prev->next = dnode_del->next;
 		dnode_del->next->prev = dnode_del->prev;
 		free(dnode_del);
+	}
+}
+
+void	ps_lstdel_all(t_dnode *dnode_start)
+{
+	t_dnode *dnode_next;
+
+	if (dnode_start->next == 0)
+		free(dnode_start);
+	else
+	{
+		dnode_next = dnode_start->next;
+		free(dnode_start);
+		ps_lstdell_all(dnode_next);
 	}
 }
