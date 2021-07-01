eaffd124b83b1f2596a18048f42d6cfa9f281a63
chichoon <chichoon.choi@gmail.com>
2021-07-01 22:27:01 +0900

    [push_swap] help me

--- 02_push_swap/pushswap/push_swap.h
+++ 02_push_swap/pushswap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 16:03:12 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/07/01 17:14:12 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/07/01 22:02:25 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ typedef struct s_dnode
 }				t_dnode;
 
 /*
+	** Push_Swap: Validate Argument
+*/
+void	ps_validate_arg(int argc, char *argv);
+
+/*
 	** Push_Swap: Command functions (sa, sb, ra, rb, pa, pb, rra, rrb)
 */
 void	ps_sa(t_dnode *a_top, t_dnode *a_end);
@@ -33,8 +38,9 @@ void	ps_pb(t_dnode *a_top, t_dnode *a_end, t_dnode *b_top);
 void	ps_ra(t_dnode *a_top, t_dnode *a_end);
 void	ps_rb(t_dnode *b_top, t_dnode *b_end);
 void	ps_rr(t_dnode *a_top, t_dnode *a_end, t_dnode *b_top, t_dnode *b_end);
-
-
+void	ps_rra(t_dnode *a_top, t_dnode *a_end);
+void	ps_rrb(t_dnode *b_top, t_dnode *b_end);
+void	ps_rrr(t_dnode *a_top, t_dnode *a_end, t_dnode *b_top, t_dnode *b_end);
 
 /*
 	** Push_Swap: function for double linked list (del, add)
---
+++ 02_push_swap/pushswap/push_swap_main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_main.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/01 20:59:24 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/07/01 22:00:18 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	push_swap()
+
+int		main(int argc, char *argv[])
+{
+	int	*num_arr;
+	if (argc > 1)
+	{
+		num_arr = ps_validate_arg(argc, argv)
+		push_swap()
+	}
+}
