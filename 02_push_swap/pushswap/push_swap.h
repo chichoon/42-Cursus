49ba43d5c8951b65d4b9d4f79f4401e19c1e308e
chichoon <chichoon.choi@gmail.com>
2021-07-01 17:27:42 +0900

    [push_swap] modify header

--- 02_push_swap/pushswap/push_swap.h
+++ 02_push_swap/pushswap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 16:03:12 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/07/01 13:58:45 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/07/01 17:14:12 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,21 @@ typedef struct s_dnode
 }				t_dnode;
 
 /*
-	** Push_Swap: function for double linked list (del, add, move)
+	** Push_Swap: Command functions (sa, sb, ra, rb, pa, pb, rra, rrb)
+*/
+void	ps_sa(t_dnode *a_top, t_dnode *a_end);
+void	ps_sb(t_dnode *b_top, t_dnode *b_end);
+void	ps_ss(t_dnode *a_top, t_dnode *a_end, t_dnode *b_top, t_dnode *b_end);
+void	ps_pa(t_dnode *a_top, t_dnode *b_top, t_dnode *b_end);
+void	ps_pb(t_dnode *a_top, t_dnode *a_end, t_dnode *b_top);
+void	ps_ra(t_dnode *a_top, t_dnode *a_end);
+void	ps_rb(t_dnode *b_top, t_dnode *b_end);
+void	ps_rr(t_dnode *a_top, t_dnode *a_end, t_dnode *b_top, t_dnode *b_end);
+
+
+
+/*
+	** Push_Swap: function for double linked list (del, add)
 */
 t_dnode	*ps_lstadd_front(int key, t_dnode *dnode_next);
 t_dnode	*ps_lstadd_back(int key, t_dnode *dnode_prev);
