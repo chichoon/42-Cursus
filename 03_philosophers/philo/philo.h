db157db739ca2e784f67f969f486c0480acf3539
chichoon <chichoon.choi@gmail.com>
2021-07-29 21:20:58 +0900

    [philosophers] philo.h 추가

--- 03_philosophers/philo/philo.h
+++ 03_philosophers/philo/philo.h
@@ -1,13 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phllo_main.c                                       :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/28 15:37:13 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/07/29 16:21:13 by jiychoi          ###   ########.fr       */
+/*   Created: 2021/07/29 15:23:02 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/07/29 16:21:05 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+
+#endif
