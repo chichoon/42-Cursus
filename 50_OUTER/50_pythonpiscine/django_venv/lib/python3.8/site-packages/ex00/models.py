73581ca1b349d0ae96b085aab0eee69f2fb1019e
chichoon <chichoon.choi@gmail.com>
2021-08-23 23:42:48 +0900

    [repo] 레포지토리 정리

--- 50_OUTER/50_pythonpiscine/django_venv/lib/python3.8/site-packages/ex00/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class ImageModel(models.Model):
-    title = models.CharField(max_length=128)
-    content = models.ImageField(upload_to='image')
