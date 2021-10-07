9b9872ab545ef7b7960da90e88a75e23abf08a06
chichoon <chichoon.choi@gmail.com>
2021-10-07 23:41:20 +0900

    [minishell] ADD: fork-pipe 예제 작성중

--- 03_minishell/example/fork_pipe/fork_pipe.c
+++ 03_minishell/example/fork_pipe/fork_pipe.c
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipe.h                                             :+:      :+:    :+:   */
+/*   fork_pipe.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/07 19:16:52 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/07 19:19:16 by jiychoi          ###   ########.fr       */
+/*   Created: 2021/10/02 18:45:39 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/10/07 22:51:34 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPE_H
-# define PIPE_H
+#include "fork_pipe.h"
 
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
+char	**pipe_getpath(char *envp[])
+{
+	int		i;
+	char	**path;
 
-#endif
+	i = -1;
+	while (envp[++i])
+	{
+		if (ft_strnstr(envp[i], "PATH=", 5))
+		{
+			path = ft_split(envp[i], ':');
+			break ;
+		}
+	}
+	i = -1;
+	while (path[++i])
+		printf("%s\n", path[i]);
+	return (path);
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	pipe_getpath(envp);
+}
--- 03_minishell/example/fork_pipe/fork_pipe.h
+++ 03_minishell/example/fork_pipe/fork_pipe.h
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fork_pipe.c                                        :+:      :+:    :+:   */
+/*   fork_pipe.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/02 18:45:39 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/07 19:31:52 by jiychoi          ###   ########.fr       */
+/*   Created: 2021/10/07 19:38:37 by jiychoi           #+#    #+#             */
+/*   Updated: 2021/10/07 22:53:08 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+#ifndef FORK_PIPE_H
+# define FORK_PIPE_H
 
-int	main(int argc, char *argv[], char *envp[])
-{
-	int	i;
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-	i = -1;
-	while (envp[++i])
-		printf("%s\n", envp[i]);
-}
+/*
+	* UTILITIES
+*/
+char	**ft_split(char const *s, char c);
+char	*ft_strnstr(const char *src, const char *to_find, size_t n);
+
+#endif
