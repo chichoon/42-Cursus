4c35c951a8a21d4dcbfd51ee5b2b96af7a1bd01c
chichoon <chichoon.choi@gmail.com>
2021-12-19 16:42:47 +0900

    [minishell] UPDATE: test_execve 함수 수정

--- 03_minishell/example/test_execve.c
+++ 03_minishell/example/test_execve.c
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 18:40:51 by jiychoi           #+#    #+#             */
-/*   Updated: 2021/10/09 20:51:10 by jiychoi          ###   ########.fr       */
+/*   Updated: 2021/12/19 16:42:23 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	char	*str[4] = {"cat", "-e", "test_getcwd.c", (char *)0};
+	char	*str[4] = {"cat", "-e", "asd", (char *)0};
 	//argv[]로 넣어줄 인자값 배열
 	//실제로 터미널에 입력해주는 인자값들은 str[1] 부터이므로 (argv와 같음)
 	//cat 등을 할 때 플래그와 인자값은 str[1], str[2]에 넣어주었다
