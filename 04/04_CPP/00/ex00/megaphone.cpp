f25600164b7b5acf6cc88337e02df5455426c0ef
chichoon <chichoon.choi@gmail.com>
2022-04-28 15:31:40 +0900

    [CPP00] UPDATE: j 초기화 제대로 안되어서 환경변수 건드리는 현상 수정

--- 04/04_CPP/00/ex00/megaphone.cpp
+++ 04/04_CPP/00/ex00/megaphone.cpp
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 15:14:50 by jiychoi           #+#    #+#             */
+/*   Updated: 2022/04/28 15:30:23 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-	int j = 0;
-
+	int j;
 	if (argc < 2) std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else {
 		for (int i = 1; i < argc; i++) {
+			j = 0;
 			while (argv[i][j]) {
 				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
 					std::cout << char(argv[i][j] - 32);
@@ -14,6 +26,6 @@ int main(int argc, char *argv[])
 				j++;
 			}
 		}
-	}
 	std::cout << "\n";
+	}
 }
