a216491ec1f673ce5e21aaf583474853882fe7e7
chichoon <chichoon.choi@gmail.com>
2022-04-28 17:57:16 +0900

    [CPP00] UPDATE: Namespace 추가

--- 04/04_CPP/00/ex01/Contact.cpp
+++ 04/04_CPP/00/ex01/Contact.cpp
@@ -6,18 +6,18 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 16:22:52 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/04/28 17:55:45 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/04/28 17:57:04 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
-void	SetDarkestSecret(string _darkestSecret)
+void	Contact::SetDarkestSecret(string _darkestSecret)
 {
 	darkestSecret = _darkestSecret;
 }
 
-string	GetDarkestSecret()
+string	Contact::GetDarkestSecret()
 {
 	return darkestSecret;
 }
