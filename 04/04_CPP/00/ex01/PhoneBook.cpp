639956fd91b6e80c5b78959b5e85fd08b83e642a
chichoon <chichoon.choi@gmail.com>
2022-05-06 15:23:44 +0900

    [CPP00] UPDATE: isFilled 배열 사용하도록 수정

--- 04/04_CPP/00/ex01/PhoneBook.cpp
+++ 04/04_CPP/00/ex01/PhoneBook.cpp
@@ -6,42 +6,41 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 16:22:07 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/05/06 12:16:45 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/05/06 15:23:35 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook(void)
-{
+PhoneBook::PhoneBook(void) {
 	curIndex = 0;
+	for (int i = 0; i < 8; i++) isFilled[i] = 0;
 }
 
-void PhoneBook::AddNewContact()
-{
+void PhoneBook::AddNewContact() {
 	std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
 
 	std::cout << "Enter Firstname: ";
-	std::cin >> firstName;
+	std::getline(std::cin, firstName);
 	std::cout << "Enter Lastname: ";
-	std::cin >> lastName;
+	std::getline(std::cin, lastName);
 	std::cout << "Enter Nickname: ";
-	std::cin >> nickName;
+	std::getline(std::cin, nickName);
 	std::cout << "Enter Phone Number: ";
-	std::cin >> phoneNumber;
+	std::getline(std::cin, phoneNumber);
 	std::cout << "Enter Darkest Secret: ";
-	std::cin >> darkestSecret;
+	std::getline(std::cin, darkestSecret);
 	contactArr[curIndex].SetFirstName(firstName);
 	contactArr[curIndex].SetLastName(lastName);
 	contactArr[curIndex].SetNickName(nickName);
 	contactArr[curIndex].SetPhoneNumber(phoneNumber);
 	contactArr[curIndex].SetDarkestSecret(darkestSecret);
+	isFilled[curIndex] = 1;
 	curIndex++;
 	if (curIndex == 8) curIndex = 0;
 }
 
-static void wordWrap(std::string str)
-{
+static void wordWrap(std::string str) {
 	int length = str.length();
 
 	if (length >= 10)
@@ -52,8 +51,7 @@ static void wordWrap(std::string str)
 	}
 }
 
-static void printContact(Contact contact, int i)
-{
+static void printContact(Contact contact, int i) {
 	std::cout << "|         ";
 	std::cout << i;
 	std::cout << "|";
@@ -65,8 +63,7 @@ static void printContact(Contact contact, int i)
 	std::cout << "|\n";
 }
 
-static void printSearchedContact(Contact contact)
-{
+static void printSearchedContact(Contact contact) {
 	std::cout << "Firstname: " << contact.GetFirstName() << "\n";
 	std::cout << "Lastname: " << contact.GetLastName() << "\n";
 	std::cout << "Nickname: " << contact.GetNickName() << "\n";
@@ -75,18 +72,17 @@ static void printSearchedContact(Contact contact)
 
 }
 
-void PhoneBook::SearchContact()
-{
+void PhoneBook::SearchContact() {
 	int searchIdx;
 
 	std::cout << "|     INDEX| FIRSTNAME|  LASTNAME|  NICKNAME|\n";
 	for (int i = 0; i < 8; i++) {
-		if (!contactArr[i].GetFirstName().length()) break;
+		if (!isFilled[searchIdx]) break;
 		printContact(contactArr[i], i);
 	}
 	std::cout << "Enter Index:";
 	std::cin >> searchIdx;
-	if (searchIdx < 0 || searchIdx > 7 || !contactArr[searchIdx].GetFirstName().length())
+	if (searchIdx < 0 || searchIdx > 7 || !isFilled[searchIdx])
 		std::cout << "No Contact Found in index No." << searchIdx << "!!!\n";
 	else printSearchedContact(contactArr[searchIdx]);
 }
