3b4a0b289a7e274f91e51a906a3e641930f4fcaf
chichoon <chichoon.choi@gmail.com>
2022-05-06 15:23:57 +0900

    [CPP00] UPDATE: 메인문 작성

--- 04/04_CPP/00/ex01/awesome_phonebook.cpp
+++ 04/04_CPP/00/ex01/awesome_phonebook.cpp
@@ -6,16 +6,21 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 17:21:26 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/05/02 16:31:02 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/05/06 15:19:06 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-int main(void)
-{
+int main(void) {
+	std::string input;
 	PhoneBook pb;
 
-	pb.AddNewContact();
-	pb.SearchContact();
+	while (1) {
+		std::cout >> "Input command: ";
+		std::getline(std::cin, input);
+		if (input === "ADD") pb.AddNewContact();
+		else if (input === "SEARCH") pb.SearchContact();
+		else if (input === "EXIT") exit(0);
+	}
 }
