0d78f0d3387f3eaf6a4cefe0d63f73fca5da75d5
chichoon <chichoon.choi@gmail.com>
2022-05-06 15:33:34 +0900

    [CPP00] RENAME: main.cpp

--- 04/04_CPP/00/ex01/main.cpp
+++ 04/04_CPP/00/ex01/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   awesome_phonebook.cpp                              :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 17:21:26 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/05/06 15:24:34 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/05/06 15:33:19 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int main(void) {
 	PhoneBook pb;
 
 	while (1) {
-		std::cout >> "Input command: ";
+		std::cout << "Input command: ";
 		std::getline(std::cin, input);
 		if (input == "ADD") pb.AddNewContact();
 		else if (input == "SEARCH") pb.SearchContact();
-		else if (input == "EXIT") exit(0);
+		else if (input == "EXIT" || std::cin.eof()) break;
+		else std::cout << "Invalid command!!\n";
 	}
+	std::cout << "Existing Phonebook...\n";
 }
