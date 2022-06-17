ba457e4965be37b3e28af6cdf9f2e68791d3d206
chichoon <chichoon.choi@gmail.com>
2022-06-17 19:03:58 +0900

    [CPP00] UPDATE: toupper 메서드로 쉽게 대문자화

--- 04/04_CPP/00/ex00/megaphone.cpp
+++ 04/04_CPP/00/ex00/megaphone.cpp
@@ -6,26 +6,19 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 15:14:50 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/04/28 15:30:23 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/05/18 17:10:39 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	int j;
+int main(int argc, char *argv[]) {
 	if (argc < 2) std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 	else {
-		for (int i = 1; i < argc; i++) {
-			j = 0;
-			while (argv[i][j]) {
-				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-					std::cout << char(argv[i][j] - 32);
-				else std::cout << argv[i][j];
-				j++;
-			}
-		}
-	std::cout << "\n";
+		for (int i = 1; i < argc; i++)
+			for(int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		std::cout << "\n";
 	}
+	return (0);
 }
